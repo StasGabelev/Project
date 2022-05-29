e7cdd13ada8bf2cc417219d7709f1417abb04cc9
Stanislav <stanislavgabelev@gmail.com>
2022-05-29 23:15:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ "use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+
+//  const result = confirm("Are you here?");
+//  console.log(result);
+
+ const answer = +prompt("Вам есть 18?", "");
+console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt("Назовите Ваше имя", "");
+// answers[1] = prompt("Назовите Вашу фамилию", "");
+// answers[2] = prompt("Назовите Ваш возраст", "");
+
+// document.write(answers);
